10f452d55163dd491c753313d8bea2c5a0e3d670
UtibeAbasi <1.BarukhOr@gmail.com>
2016-03-23 15:52:45 -0400

    Implemented Validation
    + Note: when there is an error, the application does not return a "beautiful" error

--- models/staff.js
+++ models/staff.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+const bcrypt = require('bcrypt-nodejs')
 
 //Staff Schema
 var staffSchema = new Schema({
 	name:{
-		type:String
+		type:String,
+		required:[true,'missing a name']
 	},
 	imgUrl:{
 		type:String
@@ -12,42 +14,44 @@ var staffSchema = new Schema({
 	username:{
 		type:String,
 		unique:true,
-		lowercase:true
+		lowercase:true,
+		min:[3,'User Name is too small'],
+		required:[true,'missing a username']
 	},
 	job:{
-		type:[Number]
+		type:[Number],
+		min:[1,'You need at least 1 job'],
+		required:[true,'Need to list a job']
 	},
 	password:{
-		type:String
+		type:String,
+		required:[true,'No password provided']
 	}
-});
+},{collection:'staff'}); //override default collection name
 
+
+// On Save Hook, encrypt password
+// Before saving a model, run this function
+staffSchema.pre('save',function(next){
+	//get access to the staff model
+	const staff = this;
+
+	//generate a salt then run callback
+	bcrypt.genSalt(10,function(err,salt){
+		if (err) {return next(err)}
+
+		//hash(encrypt) the password using the salt then run the callback
+		bcrypt.hash(staff.password,salt,null,function(err,hash){
+			if(err){return next(err)}
+
+			//overwrite the plain text password with encrypted password
+			staff.password = hash;
+			next();
+		})
+	})
+})
 //Create the Staff class
 const StaffClass = mongoose.model('staff',staffSchema)
 
 //Export the Staff class
 module.exports = StaffClass
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var Staff = module.exports = mongoose.model('staff',staffSchema)
-
-// //Get all staff
-// module.exports.getStaff = function(callback,limit){
-// 	Staff.find(callback).limit(limit);
-// }
-
-// module.exports.getStaffByID = function(username,callback){
-// 	Staff.findOne({'username':username},callback);
-// }
