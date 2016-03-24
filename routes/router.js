3dce512614772aed7469d59d3956796d79326ef0
UtibeAbasi <1.BarukhOr@gmail.com>
2016-03-24 13:04:34 -0400

    Implemented Signin Functionality

--- routes/router.js
+++ routes/router.js
@@ -3,7 +3,7 @@ const passportService = require('../services/passport')
 const passport = require('passport')
 
 const requireAuth = passport.authenticate('jwt',{session:false})
-
+const requreSignin = passport.authenticate('local',{session:false})
 
 
 module.exports = function(app){
@@ -13,6 +13,9 @@ module.exports = function(app){
 
 	app.post('/signup', Authentication.signup)
 	
+	app.post('/signin',requreSignin,Authentication.sigin)
+
+
 	app.get('/',function(request,response,next){
 		response.send(['watermelon','mongols','phobe'])
 		console.log(request.body)
--- services/passport.js
+++ services/passport.js
@@ -3,6 +3,40 @@ const Staff = require('../models/staff')
 const config = require('../config')
 const JwtStrategy = require('passport-jwt').Strategy
 const ExtractJwt = require('passport-jwt').ExtractJwt
+const ActiveDirectory = require('activedirectory')
+const LocalStrategy = require('passport-local')
+
+
+
+//Create local Strategy
+const localLogin = new LocalStrategy({},function(username,password,done){
+	const localConfig = {
+		url:'ldap://scsu.southernct.edu',
+		baseDN:'dc=scsu,dc=southern,dc=edu',
+		username:username.'@southernct.edu',
+		password:password
+	}
+
+	
+	const ad = new ActiveDirectory(config)
+	ad.authenticate(localConfig.username,localConfig.password,function(err,auth){
+		if(err){return done(err)}
+		if(!auth){return done(null,false)}
+		//Verify SCSU credentials
+		if(auth){
+			console.log('1st step accomplished')
+			//Verify that user is part of reslife staff
+			User.findOne({username:username},function(err,user){
+				if(err){return done(err)}
+				if(!user){return done(null,false)}
+
+				//if a user is found in the staff database with this username then return that staff member's information
+				return done(null,user)
+			})
+		}
+		
+	})
+})
 
 // Setup options for JWWT strategy
 const jwtOptions = {
@@ -28,3 +62,4 @@ const jwtLogin = new JwtStrategy(jwtOptions,function(payload,done){
 
 //Tell Passport to use this strategy
 passport.use(jwtLogin)
+passport.use(localLogin)
