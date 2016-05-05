857b55e2a39990c7fc3909d420c3c07f01d91410
UtibeAbasi <1.BarukhOr@gmail.com>
2016-05-05 14:49:43 -0400

    Minor Update
    + Added new routes, and also renamed existing ones

--- src/entry.js
+++ src/entry.js
@@ -13,19 +13,22 @@ import MainLayout from './components/main_layout';
 import Signin from './components/auth/signin';
 import Dash from './components/dash/index';
 import Graphics from './components/graphics/index';
+import Graphic_Request from './components/graphics/graphic_request';
 
 injectTapEventPlugin();
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <Router history={browserHistory}>
-    	<Route path="/" component={MainLayout}>
-    		<Route path="signin" component={Signin} />
-    		<Route path="dash" component={Dash} />
-    		<Route path="graphics/pending" component={Graphics} />
-    	</Route>
-    </Router>
-  </Provider>,document.getElementById('root')
+	<Provider store={createStoreWithMiddleware(reducers)}>
+		<Router history={browserHistory}>
+			<Route path="/" component={MainLayout}>
+				<IndexRoute component={Graphic_Request} />
+				<Route path="dash" component={Dash} />
+				<Route path="signin" component={Signin} />
+				<Route path="graphics/review" component={Graphics} />
+				<Route path="graphics/new" component={Graphic_Request} />
+			</Route>
+		</Router>
+	</Provider>,document.getElementById('root')
 )
